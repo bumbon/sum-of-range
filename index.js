2e26ecd8f5c5f413b352daf78fe6ab710c99e6f7
Ivan shyyka <i.shyyka@ukr.net>
2016-10-19 22:28:33 +0000

    Initial commit

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sum(array) {
   return total;
 }
 
-console.log(range(1, 10, 1))
+console.log(range(1, 10, 1));
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
